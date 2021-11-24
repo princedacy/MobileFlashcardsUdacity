0f506dedbdf598b6267121010c689691eda28c34
Prince Dacy <prince.dacy@codeland.it>
2021-11-24 20:16:29 +0200

    feat: add router

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+// import Router from './Router';
+import reducer from './reducers'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  render() {
+    const store = createStore(reducer)
+
+    return (
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ router/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { pink } from '../utils/colors'
+import Constants from 'expo-constants'
+import { StatusBar, View } from 'react-native'
+// import Nav from ''
+
+const Router = () => {
+    return (
+        <NavigationContainer>
+            <AppStatusbar backgroundColor={pink} barStyle={'light-content'} />
+            <Nav />
+        </NavigationContainer>
+    )
+}
+
+const AppStatusbar = ({ backgroundColor, ...props }) => {
+    return (
+        <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+            <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+        </View>
+    )
+}
+
+export default Router
